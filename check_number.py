4673b14be47fe994317cfae94bc013aed6def17e
Oscar Reyes <oscar.reyes@licklider.cl>
2024-06-09 12:07:06 -0400

    ciclo 1 - 2

--- check_number.py
+++ check_number.py
@@ -4,10 +4,12 @@ class TestCheckNumber(unittest.TestCase):
 
     def test_is_even(self):
         self.assertEqual(check_number(2), "Es par")
+        self.assertEqual(check_number(3), "No es par")
 		
 def check_number(number):
     if number % 2 == 0:
-        return "Es par"        
+        return "Es par"
+    return "No es par"
 
 if __name__ == "__main__":
     unittest.main()
