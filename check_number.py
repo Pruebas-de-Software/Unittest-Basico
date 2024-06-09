9aa13cbf1e7f49ff56051e3f35b71bb30f42d3f0
Oscar Reyes <oscar.reyes@licklider.cl>
2024-06-09 12:22:46 -0400

    ciclo 3

--- check_number.py
+++ check_number.py
@@ -9,9 +9,16 @@ class TestCheckNumber(unittest.TestCase):
     def test_is_divisible_by_3(self):
         self.assertEqual(check_number(3), "Es divisible por 3")
         self.assertNotEqual(check_number(2), "Es divisible por 3")
+    
+    def test_is_even_and_divisible_by_3(self):
+        self.assertEqual(check_number(6), "Es par y divisible por 3")
+        self.assertNotEqual(check_number(2), "Es par y divisible por 3")
+        self.assertNotEqual(check_number(3), "Es par y divisible por 3")
 		
 def check_number(number):
-    if number % 2 == 0:
+    if number % 2 == 0 and number % 3 == 0:
+        return "Es par y divisible por 3"
+    elif number % 2 == 0:
         return "Es par"
     elif number % 3 == 0:
         return "Es divisible por 3"
